6309d00db8b0c59b8c96e1eb0a6b420c1cddfb7b
Grace Kabaghe <gracema2002@gmail.com>
2026-07-17 14:41:41 +0200

    Add second quiz question (spider legs) - practicing if/else pattern

--- quiz.py
+++ quiz.py
@@ -4,3 +4,9 @@ if answer == "green":
     print("Correct! You're a natural.")
 else:
     print("Not quite! Mixing blue and yellow makes green!")
+
+answer2 = input("How many legs does a spider have? ")
+if answer2 == "8":
+    print("Correct! Spiders have 8 legs.")
+else:
+    print("Not quite! Spiders have 8 legs.")
