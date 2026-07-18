52fc819311574af1defe3b7ec3d9fd6b80973b00
Grace Kabaghe <gracema2002@gmail.com>
2026-07-18 18:17:27 +0200

    Session 4: refactor quiz with ask_question() function + function practice file

---
+++ functions_practice.py
@@ -0,0 +1,5 @@
+def congratulate(name):
+    print("Well done, " + name + "!")
+    print("You're learning to code!")
+congratulate("Grace")
+congratulate("Amara")
--- quiz.py
+++ quiz.py
@@ -1,12 +1,26 @@
+# print("Quiz time! Let's test what you know.")
+# questions = ["what color do you get by mixing blue with yellow? ", 
+# "how many legs does a spider have? "]
+# answers = ["green", "8"]
+
+# for i in range(len(questions)):
+#     reply = input(questions[i] + " ")
+#     if reply == answers[i]:
+#         print("Correct")
+#     else:
+#         print("Not quite! The answer was " + answers[i])
+
 print("Quiz time! Let's test what you know.")
-questions = ["what color do you mix by mixing blue with yellow? ", 
-"how many legs dord s spider have? "]
+questions = ["what color do you get by mixing blue with yellow? ", 
+"How many legs does a spider have? "]
 answers = ["green", "8"]
 
-for i in range(len(questions)):
-    reply = input(questions[i] + " ")
-    if reply == answers[i]:
+def ask_question(question, correct_answer):
+    reply = input(question + " ")
+    if reply == correct_answer:
         print("Correct")
     else:
-        print("Not quite! The answer was " + answers[i])
+        print("Not quite! The answer was " + correct_answer) 
 
+for i in range(len(questions)):
+    ask_question(questions[i], answers[i])
