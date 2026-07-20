d0398d111559bad73bc2080f84752963454ab28b
Grace Kabaghe <gracema2002@gmail.com>
2026-07-20 15:24:44 +0200

    Session 5: rewrite quiz with dictionary (key-value pairs) + .items() loop

--- quiz.py
+++ quiz.py
@@ -11,16 +11,33 @@
 #         print("Not quite! The answer was " + answers[i])
 
 print("Quiz time! Let's test what you know.")
-questions = ["what color do you get by mixing blue with yellow? ", 
-"How many legs does a spider have? "]
-answers = ["green", "8"]
+# questions = ["what color do you get by mixing blue with yellow? ", 
+# "How many legs does a spider have? "]
+# answers = ["green", "8"]
+
+# def ask_question(question, correct_answer):
+#     reply = input(question + " ")
+#     if reply == correct_answer:
+#         print("Correct")
+#     else:
+#         print("Not quite! The answer was " + correct_answer) 
+
+# for i in range(len(questions)):
+#     ask_question(questions[i], answers[i])
+
+quiz = {
+    "What color do you get by mixing blue with yello?":
+        "Green",
+    "How many legs does a spider have?":
+        "8"
+    }
 
 def ask_question(question, correct_answer):
     reply = input(question + " ")
     if reply == correct_answer:
         print("Correct")
     else:
-        print("Not quite! The answer was " + correct_answer) 
+        print("Not quite! The correct answer was " + correct_answer)
 
-for i in range(len(questions)):
-    ask_question(questions[i], answers[i])
+for question, answer in quiz.items():
+    ask_question(question, answer)
