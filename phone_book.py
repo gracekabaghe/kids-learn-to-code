f65dac8ac5ab1d71d585914034f7474880073ca8
Grace Kabaghe <gracema2002@gmail.com>
2026-07-20 17:53:26 +0200

    Challenge: phone_book - dictionary .items() loop + direct key lookup

---
+++ phone_book.py
@@ -0,0 +1,10 @@
+contacts = {
+    "Grace": "097 123 456",
+    "Amara": "096 234 567",
+    "David": "095 345 678",
+    "Faith": "094 456 789"
+}
+for name, number in contacts.items():
+    print(name, " ", number)
+
+print("Grace's number is ", contacts["Grace"])
