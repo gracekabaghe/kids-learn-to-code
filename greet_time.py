f377c76783c965a3772fa102ba2c17af60bbbb8e
Grace Kabaghe <gracema2002@gmail.com>
2026-07-23 14:50:12 +0200

    Challenge: greet_time - if/elif/elif/else multi-branch logic

---
+++ greet_time.py
@@ -0,0 +1,9 @@
+time = input("What time of the day is it? (morning/afternoon/evening): ")
+if time == "morning":
+    print("Good morning, Ready to code")
+elif time == "afternoon":
+    print("Good afternoon, keep going")
+elif time == "evening":
+    print("Good evening. Time to wind down")
+else:
+    print("I don't know what time of the day it is")
